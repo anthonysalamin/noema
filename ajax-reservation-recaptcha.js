43c80907daa18ab0078768ccdfefa62d32ca24b4
Anthony Salamin <37147870+anthonysalamin@users.noreply.github.com>
2021-10-26 22:00:04 +0200

    Update ajax-reservation-recaptcha.js

--- ajax-reservation-recaptcha.js
+++ ajax-reservation-recaptcha.js
@@ -1,7 +1,6 @@
-console.clear();
 /*
  * 🔴 NOEMA | AJAX reservation + invisible recaptcha (V2) V.07
- * build: 26.10.2021 21:21 | anthonysalamin.ch
+ * build: 26.10.2021 21:58 | anthonysalamin.ch
  */
 console.log("loaded AJAX + invisible recaptcha (V2) V.07");
 const log = console.log,
